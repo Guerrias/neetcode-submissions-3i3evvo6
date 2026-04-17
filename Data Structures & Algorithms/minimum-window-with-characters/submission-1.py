4fdfcc7b0e719e01e7f519e479eef15a8ede0a30
Toussida <49766172+Guerrias@users.noreply.github.com>
2026-04-17 15:39:54 -0500

    Add: minimum-window-with-characters - submission-1

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-1.py
@@ -0,0 +1,43 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        if len(t) > len(s) :
+             return ""
+        
+        freqT = {}
+        freq = {}
+
+        for char in t :
+            freqT[char] = freqT.get(char, 0) + 1
+        
+        left = 0
+        right = 0
+        minWindowSize = len(s) + 1
+        minLeft = -1
+        minRight = -1
+
+        #print(freqT)
+        count = 0
+        while right < len(s) :
+            char = s[right]
+            if char in freqT :
+                freq[char] = freq.get(char, 0) + 1
+                if freq[char] == freqT[char] :
+                    count += 1
+            #print(freq)
+            while count == len(freqT):
+                #print(f"{minWindowSize} {right - left + 1}")
+                if minWindowSize > right - left + 1 :
+                    minWindowSize = right - left + 1
+                    minLeft = left
+                    minRight = right
+                
+                charToRemove = s[left]
+                left += 1
+
+                if charToRemove in freq :
+                    freq[charToRemove] = freq.get(charToRemove, 0) -1
+                    if freq[charToRemove] < freqT[charToRemove] :
+                        count -=1
+                
+            right += 1
+        return s[minLeft:minRight+1] if minLeft != -1 and minRight != -1 else ""
