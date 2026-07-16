03080dff1a1b7454c0ba416d6d1b01f1b8903c89
Toussida <49766172+Guerrias@users.noreply.github.com>
2026-07-16 10:41:56 -0500

    Add: house-robber-ii - submission-7

---
+++ Data Structures & Algorithms/house-robber-ii/submission-7.py
@@ -0,0 +1,19 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if len(nums) == 1:
+            return nums[0]
+            
+        return max(self.helper(nums[1:]), self.helper(nums[:-1]))
+    
+    def helper(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+        
+        n = len(nums)
+        maxAmount = [0] * (n+1)
+        maxAmount[1] = nums[0]
+
+        for i in range(2, n+1):
+            maxAmount[i] = max(maxAmount[i-1], maxAmount[i-2] + nums[i-1])
+        
+        return maxAmount[n]
